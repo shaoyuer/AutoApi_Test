30f274d3be3c403297b1d8d672076a621e316dd2
wangziyingwen <wz.lxh@outlook.com>
2021-02-07 22:03:41 +0800

    Update ApiOfWrite.py

--- ApiOfWrite.py
+++ ApiOfWrite.py
@@ -74,7 +74,7 @@ for a in range(1, int(app_num)+1):
 headers={'Accept-Language': 'zh-CN',
          'User-Agent': r"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36" }
 weather=req.get(r'http://wttr.in/'+city,headers=headers)
-weather=weather.content.decode('utf-8'))
+weather=weather.content.decode('utf-8')
         
 for a in range(1, int(app_num)+1):
     UploadFile(a)
