d77d9aa24b21ea3f079c60c6aee6b0ccae09dd23
wangziyingwen <wz.lxh@outlook.com>
2021-02-08 17:55:32 +0800

    Update ApiOfWrite.py

--- ApiOfWrite.py
+++ ApiOfWrite.py
@@ -3,7 +3,6 @@ import os
 import requests as req
 import xlsxwriter
 import json,sys,time,random
-
 reload(sys)
 sys.setdefaultencoding('utf-8')
 emailaddress=os.getenv('EMAIL')
@@ -16,7 +15,6 @@ city=os.getenv('CITY')
 if city == '':
     city = 'Beijing'
 access_token_list=['wangziyingwen']*int(app_num)
-
 #微软refresh_token获取
 def getmstoken(ms_token,appnum):
     headers={'Content-Type':'application/x-www-form-urlencoded'
@@ -35,8 +33,7 @@ def getmstoken(ms_token,appnum):
         print(r'账号/应用 '+str(appnum)+' 的微软密钥获取失败\n'+'请检查secret里 CLIENT_ID , CLIENT_SECRET , MS_TOKEN 格式与内容是否正确，然后重新设置')
     refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
-    return access_token
-    
+    return access_token    
 #apiPost函数
 def apiPost(a,data,url):
     access_token=access_token_list[a-1]
@@ -50,7 +47,6 @@ def apiPost(a,data,url):
     else:
         print('    操作失败')
     return posttext
-
 #apiDelete函数
 def apiDelete(a,url):
     access_token=access_token_list[a-1]
@@ -77,8 +73,7 @@ def SendEmail(a,subject,content):
                              'toRecipients': [{'emailAddress': {'address': email_address}}],
                              },
                  'saveToSentItems': 'true'}            
-    apiPost(a,json.dumps(mailmessage),url)
-		
+    apiPost(a,json.dumps(mailmessage),url)		
 #修改excel(这函数分离好像意义不大)
 #api-获取itemid: https://graph.microsoft.com/v1.0/me/drive/root/search(q='.xlsx')?select=name,id,webUrl
 def excelWrite(a,filesname,sheet):
@@ -95,7 +90,6 @@ def excelWrite(a,filesname,sheet):
          }
     print('  添加表格')
     apiPost(a,data,url)
-
 def taskWrite(a,taskname):
     url=r'https://graph.microsoft.com/v1.0/me/todo/lists'
     data={
@@ -110,9 +104,7 @@ def taskWrite(a,taskname):
     url=r'https://graph.microsoft.com/v1.0/me/todo/lists/'+listjson['id']+r'/tasks/'+taskjson['id']
     apiDelete(a,url)
     url=r'https://graph.microsoft.com/v1.0/me/todo/lists'+listjson['id']
-    apiDelete(a,url)
-    
-    
+    apiDelete(a,url)    
 def teamWrite(a,channelname):
     access_token=access_token_list[a-1]
     headers={
@@ -133,19 +125,16 @@ def teamWrite(a,channelname):
     jsontxt = json.loads(apiPost(a,data,url))
     url=r'https://graph.microsoft.com/v1.0/teams/'+objectlist[0]['id']+r'/channels/'+jsontxt['id']
     print("  删除team频道")
-    apiDelete(a,url) 
-     
+    apiDelete(a,url)    
 #一次性获取access_token，降低获取率
 for a in range(1, int(app_num)+1):
     client_id=os.getenv('CLIENT_ID_'+str(a))
     client_secret=os.getenv('CLIENT_SECRET_'+str(a))
     ms_token=os.getenv('MS_TOKEN_'+str(a))
     access_token_list[a-1]=getmstoken(ms_token,a)
-
 #获取天气
 headers={'Accept-Language': 'zh-CN'}
 weather=req.get(r'http://wttr.in/'+city+r'?format=4&?m',headers=headers).text
-
 #实际运行   
 for a in range(1, int(app_num)+1):
     #生成随机名称
@@ -165,7 +154,6 @@ for a in range(1, int(app_num)+1):
     print('发送邮件')
     if emailaddress != '':
         SendEmail(a,'weather',weather)
-    #excel,team,task3选1执行
     choosenum = random.randint(1,3) 
     print('excel文件操作')
     excelWrite(a,filesname,'QVQ'+str(random.randint(1,600))
