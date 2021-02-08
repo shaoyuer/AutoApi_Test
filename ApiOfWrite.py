e59ac9ce4c255114f523b30f413748c8e5ef9765
wangziyingwen <wz.lxh@outlook.com>
2021-02-08 17:25:31 +0800

    Update ApiOfWrite.py

--- ApiOfWrite.py
+++ ApiOfWrite.py
@@ -173,7 +173,7 @@ for a in range(1, int(app_num)+1):
     elif config == 'Y' or choosenum == 2  :
         print('team频道操作')
         teamWrite(a,'QVQ'+str(random.randint(1,600))
-    elif config == 'Y' or choosenum == 3 :
+    else config == 'Y' or choosenum == 3 :
         print('tasks操作')
         taskWrite(a,'QVQ'+str(random.randint(1,600))
     
