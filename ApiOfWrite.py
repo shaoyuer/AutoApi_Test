3372115ef763f0a5ccb48b4d68a320a0dd4b8e44
wangziyingwen <wz.lxh@outlook.com>
2021-02-21 22:36:46 +0800

    Update ApiOfWrite.py

--- ApiOfWrite.py
+++ ApiOfWrite.py
@@ -316,7 +316,7 @@ if other_config['tg_bot'] != ['','']:
     content='AutoApi.W简报: '+'\n'
     for i in range(app_count):
         if log_list[i] != '':
-            content=content+'账号 '+str(i)+' 失败api：'+log_list[i]
+            content=content+'账号 '+str(i)+' 失败api：'+log_list[i]+'\n'
         else:
-            content=content+'账号 '+str(i)+'所有api : OK'
+            content=content+'账号 '+str(i)+'所有api : OK'+'\n'
     sendTgBot(content)
